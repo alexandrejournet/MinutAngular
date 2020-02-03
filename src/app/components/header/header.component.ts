46193bc4c366676261437b628e22cc42a8e031ae
Alexandre <alexandre.journet@gmail.com>
2020-02-03 20:44:07 +0100

    Full body

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() public sidenavToggle = new EventEmitter();
- 
   constructor() { }
  
   ngOnInit() {
-  }
- 
-  public onToggleSidenav = () => {
-    this.sidenavToggle.emit();
   }
 
 }
